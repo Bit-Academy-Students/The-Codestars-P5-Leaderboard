d0f81a4af5b42097a38ea49b61355d4c818703aa
Julius <pecoraio.j@gmail.com>
2025-11-26 11:21:07 +0100

    achtergrond kleur aangepast

--- prototype1/src/App.jsx
+++ prototype1/src/App.jsx
@@ -3,14 +3,14 @@ import Mainpage from "./Mainpage";
 import Login from "./Login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Mainpage />} />
-        <Route path="/Login-Prototype" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Mainpage />} />
+                <Route path="/Login-Prototype" element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- prototype1/src/Login.jsx
+++ prototype1/src/Login.jsx
@@ -1,20 +1,22 @@
 import { Link } from "react-router-dom"
 
 function Login() {
-  return (
-    <div className="login-container">
-      <h2>Login</h2>
-      <form action="/login" method="POST">
-        <label htmlFor="email">email:</label>
-        <input type="email" name="email" id="email" />
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+            <form action="/login" method="POST">
+                <label htmlFor="email">email:</label>
+                <input type="email" name="email" id="email" />
 
-        <label htmlFor="password">password:</label>
-        <input type="password" name="password" id="password" />
+                <label htmlFor="password">password:</label>
+                <input type="password" name="password" id="password" />
 
-        <input type="submit" value="Login" />
-      </form>
-      <p><Link to="/register">Don't have an account? Register here!</Link></p>
-    </div>
-  )
+                <input type="submit" value="Login" />
+            </form>
+            <p>
+                <Link to="/register">Don't have an account? Register here!</Link>
+            </p>
+        </div>
+    )
 }
 export default Login
